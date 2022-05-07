c8e0ace3e52ce2ab12cff4e40b9a4160b2a02cc9
I3ismut0 <102481682+I3ismut0@users.noreply.github.com>
2022-05-07 10:39:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 import sphinx
 import subprocess
 from hashdoc import valorhash
+#from scraping import 
 
 def main():
     parser = argparse.ArgumentParser(
@@ -11,10 +12,13 @@ def main():
         epilog="Mucho textoxd")
     parser.add_argument('-vh',
                         help='Obtiene el valor hash de todos los archivos contenidos en esta carpeta.')
+    parser.add_argument( "-s",
+                        help = "")
+    parser.add_argument("-v",
+                        help= "Verifica los correos electronicos de una lista previoamente generada.")
 
     args = parser.parse_args()
-    parser.print_help()
-    
+    parser.print_help()    
 
 if __name__ =="__main__":
     main()
