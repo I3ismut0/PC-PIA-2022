a5b40c242057652aeb63cf318387632a3a2be0b6
I3ismut0 <102481682+I3ismut0@users.noreply.github.com>
2022-05-07 00:26:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 #Recuerda importar las librerias
+import argparse
+import logging
+import sphinx
+import subprocess
+#from hashdoc import valorhash
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="suite de seguridad",
+        epilog="Mucho textoxd")
+    parser.add_argument('-vh', '--valorhash',
+                        help='Obtiene el valor hash de todos los archivos contenidos en esta carpeta.')
+
+    args = parser.parse_args()
+    parser.print_help()
+
+if __name__ =="__main__":
+    main()
