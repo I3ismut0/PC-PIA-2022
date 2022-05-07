d887fabe16561ce5fdca62dce8da5523463caa79
AbaddonIV <78581187+Felpa23@users.noreply.github.com>
2022-05-07 18:04:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,26 @@ import logging
 import sphinx
 import subprocess
 from hashdoc import valorhash
-#from scraping import 
-
+from scraping import scraping_url
+from verificacion import email_verf
 def main():
     parser = argparse.ArgumentParser(
         description="suite de seguridad",
-        epilog="Mucho textoxd")
-    parser.add_argument('-vh',
+        epilog="HASH:  ,WEBSCRAPING: Para esta opcion se necesita la url de una pagina web, VERIFICACION DE CORREO: se necesita un correo que este dentro del archivo generado por el webscraping")
+    parser.add_argument('-hash','-vh',
                         help='Obtiene el valor hash de todos los archivos contenidos en esta carpeta.')
-    parser.add_argument( "-s",
-                        help = "")
-    parser.add_argument("-v",
-                        help= "Verifica los correos electronicos de una lista previoamente generada.")
+    parser.add_argument('-webscraping', "-s",
+                        help = "Obtener correos de una pagina web")
+    parser.add_argument('-verificacion',"-v",
+                        help= "Verifica un correo del archivo previamente generado\n -Se necesita un correo del archivo generado")
 
-    args = parser.parse_args()
-    parser.print_help()    
+    if __name__ =="__main__":
+        args = parser.parse_args()
 
-if __name__ =="__main__":
-    main()
+        if args.hash:
+            valorhash()
+        elif args.webscraping:
+            scraping_url(args.webscraping)
+        elif args.verificacion:
+            email_verf(args.verificacion)
+main()
