24108e5bcd5c7c5b5870704ce6388b2d4cb8b8d6
I3ismut0 <102481682+I3ismut0@users.noreply.github.com>
2022-05-07 09:48:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import argparse
 import logging
 import sphinx
 import subprocess
-#from hashdoc import valorhash
+from hashdoc import valorhash
 
 def main():
     parser = argparse.ArgumentParser(
         description="suite de seguridad",
         epilog="Mucho textoxd")
-    parser.add_argument('-vh', '--valorhash',
+    parser.add_argument('-vh',
                         help='Obtiene el valor hash de todos los archivos contenidos en esta carpeta.')
 
     args = parser.parse_args()
     parser.print_help()
+    
 
 if __name__ =="__main__":
     main()
