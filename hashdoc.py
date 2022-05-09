a42fd07d33d9d22d0e875a630c09f6c67f33f054
I3ismut0 <102481682+I3ismut0@users.noreply.github.com>
2022-05-09 00:41:57 -0500

    Update hashdoc.py

--- hashdoc.py
+++ hashdoc.py
@@ -10,15 +10,27 @@ with os.scandir('./') as ficheros:
 temp.remove(".git")
 temp.remove("__pycache__")
 
-def valorhash():
-    f = open ('hash.txt','w')
-    for path in temp:
-        file_obj= open (path,"rb")
+def valorhash(pathuno):
+    if len(pathuno) < len(temp):
+        f = open ('hash.txt','w')
+        for path in temp:
+            file_obj= open (path,"rb")
+            file= file_obj.read()
+            Hash = hashlib.sha512(file)
+            Hashed = Hash.hexdigest()
+            f.write(path+":"+os.linesep+Hashed+ os.linesep)
+        print("fin")
+        f.close()
+    else:
+        print(pathuno)
+        f = open ('hashselected.txt','w') 
+        file_obj= open (pathuno,"rb")
         file= file_obj.read()
         Hash = hashlib.sha512(file)
         Hashed = Hash.hexdigest()
-        f.write(path+":"+os.linesep+Hashed+ os.linesep)
-    print("fin")
-    f.close()
-
-valorhash()
+        f.write(pathuno+":"+os.linesep+Hashed+ os.linesep)
+        print("fin")
+        f.close()
+        
+#pathuno = input("incerte ruta de un archivo en especifico\n")
+#valorhash(pathuno)
