f79b1714eba04155b3c7ea1333eb4a5bb4eb057c
I3ismut0 <102481682+I3ismut0@users.noreply.github.com>
2022-05-09 00:42:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,25 +12,29 @@ from verip import ipleef
 from virustotal import total
 def main():
     parser = argparse.ArgumentParser(
-        description="suite de seguridad",
-        epilog="HASH:  ,WEBSCRAPING: Para esta opcion se necesita la url de una pagina web, VERIFICACION DE CORREO: se necesita un correo que este dentro del archivo generado por el webscraping, VER PUERTOS ABIERTOS: se necesita una direccione ip la cual puedes buscar con el comando para ver ip's disponibles")
-    parser.add_argument('-hash','-vh',
+        description="Suite de seguridad",
+        epilog=
+        "HASH: WEBSCRAPING: Para esta opcion se necesita la url de una pagina web,\
+        VERIFICACION DE CORREO: se necesita un correo que este dentro del archivo generado por el webscraping\
+        VER PUERTOS ABIERTOS: se necesita una direccione ip la cual puedes buscar con el comando para ver ip's disponibles")
+
+    parser.add_argument('--hash','-vh',
                         help='Obtiene el valor hash de todos los archivos contenidos en esta carpeta.')
-    parser.add_argument('-webscraping', "-s",
+    parser.add_argument('--webscraping', "-s",
                         help = "Obtener correos de una pagina web")
-    parser.add_argument('-verificacion',"-v",
+    parser.add_argument('--verificacion',"-v",
                         help= "Verifica un correo del archivo previamente generado")
-    parser.add_argument('-verip',"-ip", action='store_true', default=False,
+    parser.add_argument('--verip',"-ip", action='store_true', default=False,
                         help='Ver direcciones ip disponibles')
-    parser.add_argument('-socket',"-so",
+    parser.add_argument('--socket',"-so",
                         help='Ver puertos abiertos y cerrados de una direccion ip')
-    parser.add_argument('-virus',"-vi",
+    parser.add_argument('--virus',"-vi",
                         help='Checar si un archivo es malicioso')
     if __name__ =="__main__":
         args = parser.parse_args()
 
         if args.hash:
-            valorhash()
+            valorhash(args.hash)
         elif args.webscraping:
             scraping_url(args.webscraping)
         elif args.verificacion:
